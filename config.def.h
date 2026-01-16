7d82c6d239c431decafcfb33dbd4f0598db84391
gawmk <mikolaj.gawrys@gmail.com>
2026-01-17 00:15:17 +0100

    kb: swap quit and killclient

--- config.def.h
+++ config.def.h
@@ -95,7 +95,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_e,      quit,                 {0} },
 };
 
 /* button definitions */
